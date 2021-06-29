f3a6f1e2dfb2d3408d1f348d71f2a91fe026d1a7
SHAIK MOHAMMED FEROZ <86046024+Feroz-Shaik-17@users.noreply.github.com>
2021-06-29 10:42:06 +0530

    Add files via upload

---
+++ multi file/fun.h
@@ -0,0 +1,24 @@
+/**
+ * @file fun.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-06-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __FUN_H__
+#define __FUN_H__
+/**
+ * @brief 
+ * 
+ * @param number1 
+ * @param number2 
+ * @return int 
+ */
+int sum(int number1, int number2);
+int square(int number);
+
+#endif 
